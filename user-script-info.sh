7515eb721645a78ee6a90c1252f1b56b320aa4a7
lwdwhen <lwdwhen@gmail.com>
2024-10-28 05:37:19 +0000

    update script to actually include user data

--- user-script-info.sh
+++ user-script-info.sh
@@ -1,1 +1,6 @@
-rm -rf ./minify
+script_info=$(grep -B 100 '^// ==/UserScript==' ./rule34_xxx/index.js)
+
+echo "${script_info}" > ./minify/rule34_xxx.min.js
+cat ./minify/tmp/rule34_xxx/index.js >> ./minify/rule34_xxx.min.js
+
+rm ./minify/tmp/rule34_xxx/index.js
